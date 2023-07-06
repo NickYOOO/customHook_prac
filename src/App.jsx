8d221d100c8e87bb75e8c75e71c39a7902972a54
Nick YOOO <outgrowthr@gmail.com>
2023-07-06 19:18:59 +0900

    Study: custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useInput from './hooks/useInput';
 
 function App() {
-  return <div>App</div>;
+  const [name, onChangeNameHandler] = useInput();
+  const [pw, onChangePWHandler] = useInput();
+
+  // const [name, setName] = useState('');
+  // const [pw, setPw] = useState('');
+
+  // const onChangeNameHandler = (e) => {
+  //   setName(e.target.value);
+  // };
+  // const onChangePWHandler = (e) => {
+  //   setPw(e.target.value);
+  // };
+
+  return (
+    <div>
+      <input type="text" value={name} onChange={onChangeNameHandler} />
+      <input type="password" value={pw} onChange={onChangePWHandler} />
+    </div>
+  );
 }
 
 export default App;
